c3e1f616c12b728fb506a606d6ee2e17f0ca4d15
Hayato Nagai <hayato@Hayatos-MacBook-Air.local>
2026-07-01 13:29:04 -0400

    Adjusted model parameters

--- create_models.py
+++ create_models.py
@@ -26,6 +26,24 @@ def split_data(X, Y):
     return train_X, test_X, train_Y, test_Y
 
 
+def evaluate_model(y_true, y_pred, objective_fn):
+    """
+    Evaluates the given model according to the provided objective function
+
+    Params:
+        y_true (Series): the actual y values
+        y_pred (Series): the predicted y values
+        objective_fn: a function that takes the model's (true neg, false pos, false neg, true pos)
+                        as input and outputs a float value
+
+    Returns: a float of the model's score under the given objective function
+    """
+    # find the nubmer of tp, fp, tn, and fn
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+
+    return objective_fn(tn, fp, fn, tp)
+
+
 def find_best_models(linear_X, tree_X, linear_Y, tree_Y, objective_fn, num_trials):
     """
     Takes in the data and runs the all four models with every possible combinatoon of each parameter
@@ -71,24 +89,6 @@ def find_best_models(linear_X, tree_X, linear_Y, tree_Y, objective_fn, num_trial
 #######################################################################################################
 
 
-def evaluate_model(y_true, y_pred, objective_fn):
-    """
-    Evaluates the given model according to the provided objective function
-
-    Params:
-        y_true (Series): the actual y values
-        y_pred (Series): the predicted y values
-        objective_fn: a function that takes the model's (true neg, false pos, false neg, true pos)
-                        as input and outputs a float value
-
-    Returns: a float of the model's score under the given objective function
-    """
-    # find the nubmer of tp, fp, tn, and fn
-    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
-
-    return objective_fn(tn, fp, fn, tp)
-
-
 def search_model(val_X, val_Y, params, objective_fn, num_trials, model):
     """
     Finds the optimal parameters that gives the best score and creates a model based on these parameters
@@ -110,7 +110,7 @@ def search_model(val_X, val_Y, params, objective_fn, num_trials, model):
     scorer = make_scorer(evaluate_model, objective_fn=objective_fn)
 
     # uses randomized search CV to find the optimal parameter conditions
-    search = RandomizedSearchCV(model, param_distributions=params, n_iter=num_trials, scoring=scorer, n_jobs = -1, cv=5, random_state=42)
+    search = RandomizedSearchCV(model, param_distributions=params, n_iter=num_trials, scoring=scorer, n_jobs = -1, cv=3, random_state=42)
 
     # uses the optimal model and fits to data
     search.fit(val_X, val_Y)
@@ -140,7 +140,7 @@ def logistic_regression(val_X, val_Y, objective_fn, num_trials):
                                {0: 1, 1: 3}, {0: 1, 1: 4}, {0: 1, 1: 5}]}
 
     # finds the best model, its parameters, and its score
-    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, LogisticRegression())
+    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, LogisticRegression(random_state=42))
 
     return best_model, parameters, score
 
@@ -162,13 +162,14 @@ def random_forest(val_X, val_Y, objective_fn, num_trials):
                 best_model is the best model given the set of parameters, and measured by the score
     """
     # the parameters to test
-    params = {"n_estimators": [100, 200, 300],
+    params = {"n_estimators": [100, 200, 500],
+              "max_features": ["sqrt", "log2", None],
               "max_depth": [None, 10, 20, 30],
               "min_samples_leaf": [1, 5, 10],
               "class_weight": ["balanced", "balanced_subsample", None]}
 
     # finds the best model, its parameters, and its score
-    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, RandomForestClassifier())
+    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, RandomForestClassifier(random_state=42))
 
     return best_model, parameters, score
 
@@ -193,10 +194,11 @@ def hist_grad_boost(val_X, val_Y, objective_fn, num_trials):
     params = {"learning_rate": [0.01, 0.05, 0.1, 0.3],
               "max_iter": [100, 200, 300],
               "max_depth": [None, 3, 9, 15],
+              "max_leaf_nodes": [31, 62, 124],
               "min_samples_leaf": [10, 20, 30]}
 
     # finds the best model, its parameters, and its score
-    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, HistGradientBoostingClassifier())
+    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, HistGradientBoostingClassifier(random_state=42))
 
     return best_model, parameters, score
 
@@ -221,10 +223,12 @@ def x_grad_boost(val_X, val_Y, objective_fn, num_trials):
     params = {"learning_rate": [0.01, 0.05, 0.1, 0.3],
               "n_estimators": [100, 200,300],
               "max_depth": [3, 6, 9],
+              "subsample": [0.7, 0.9, 1.0],
+              "colsample_bytree": [0.7, 0.9, 1.0],
               "scale_pos_weight": [1, 2, 4]}
 
     # finds the best model, its parameters, and its score
-    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, XGBClassifier())
+    best_model, parameters, score = search_model(val_X, val_Y, params, objective_fn, num_trials, XGBClassifier(random_state=42))
 
     return best_model, parameters, score
 
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import data_extraction
 import create_models
-import graph_visualization
+# import graph_visualization
 
 from data_extraction import load_data, simplify_data, tree_data, linear_data
-from create_models import split_data, find_best_models, get_accuracy, get_precision, get_recall, get_f1_score, get_balanced_accuracy, get_mcc
-from graph_visualization import plot_roc_auc, plot_pr_auc
+from create_models import split_data, evaluate_model, find_best_models, get_accuracy, get_precision, get_recall, get_f1_score, get_balanced_accuracy, get_mcc
+# from graph_visualization import plot_roc_auc, plot_pr_auc
 
 
 # extracts data for both lienar and tree models
@@ -14,16 +14,50 @@ linear_x, y = linear_data(simplify_data(load_data("data/data2025anon.csv")))
 tree_x, y = tree_data(simplify_data(load_data("data/data2025anon.csv")))
 
 # split both data types
-train_linear_x, train_linear_y, test_linear_x, test_linear_y = split_data(linear_x, y)
-train_tree_x, train_tree_y, test_tree_x, test_tree_y = split_data(tree_x, y)
+train_linear_x, test_linear_x, train_linear_y, test_linear_y = split_data(linear_x, y)
+train_tree_x, test_tree_x, train_tree_y,  test_tree_y = split_data(tree_x, y)
 
 # define the objective function
-objective_fn = get_f1_score()
+objective_fn = get_f1_score
 
 # define the number of trials
-num_trials = 100
+num_trials = 10
 
 # find the best model for each of the four learning model
 results = find_best_models(train_linear_x, train_tree_x, train_linear_y, train_tree_y, objective_fn, num_trials)
 
 print(results)
+
+# find logistic regression model
+lin_model = results[0][0]
+
+# predict and evaluate
+y_lin_pred = lin_model.predict(test_linear_x)
+
+print("Logisitc Regression Model")
+print("Accuracy: ", evaluate_model(test_linear_y, y_lin_pred, get_accuracy))
+print("Precision: ", evaluate_model(test_linear_y, y_lin_pred, get_precision))
+print("Recall: ", evaluate_model(test_linear_y, y_lin_pred, get_recall))
+print("F1 Score: ", evaluate_model(test_linear_y, y_lin_pred, get_f1_score))
+print("Balanced Accuracy: ", evaluate_model(test_linear_y, y_lin_pred, get_balanced_accuracy))
+print("MCC Score: ", evaluate_model(test_linear_y, y_lin_pred, get_mcc))
+
+# track names of tree models
+tree_names = ["Random Forest Model", "Histogram-Based Gradient Boosting Model", "Extreme Gradient Boosting Model"]
+
+# loops through all 3 tree-based models
+for i in range(1, 4):
+
+    # find the current tree based model
+    model = results[i][0]
+
+    # predict and evaluate
+    y_pred = model.predict(test_tree_x)
+
+    print(tree_names[i-1])
+    print("Accuracy: ", evaluate_model(test_tree_y, y_pred, get_accuracy))
+    print("Precision: ", evaluate_model(test_tree_y, y_pred, get_precision))
+    print("Recall: ", evaluate_model(test_tree_y, y_pred, get_recall))
+    print("F1 Score: ", evaluate_model(test_tree_y, y_pred, get_f1_score))
+    print("Balanced Accuracy: ", evaluate_model(test_tree_y, y_pred, get_balanced_accuracy))
+    print("MCC Score: ", evaluate_model(test_tree_y, y_pred, get_mcc))
