0ce2b5963db0a8223f89b8da93728e61c6528b44
Hayato Nagai <nagaihayato01@gmail.com>
2026-07-08 15:23:47 -0400

    Added thresholds and inbalanced learn

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import numpy as np
+from imblearn.over_sampling import SMOTENC
 import data_extraction
 import create_models
 import graph_visualization
 
 from data_extraction import load_data, simplify_data, tree_data, linear_data
-from create_models import split_data, find_best_models, print_stats, get_balanced_accuracy, get_precision, get_recall, get_f1_score, get_mcc
+from create_models import split_data, find_best_models, print_stats, get_balanced_accuracy, get_precision, get_recall, get_f1_score, get_mcc, evaluate_model
 from graph_visualization import plot_confusion_matrix, plot_roc_auc, plot_pr_auc, plot_feature_importance
 
 
@@ -14,6 +16,13 @@ tree_x, y = tree_data(simplify_data(load_data("data/df2023-2026_anon.csv")))
 # split both data types
 train_linear_x, test_linear_x, train_linear_y, test_linear_y = split_data(linear_x, y)
 train_tree_x, test_tree_x, train_tree_y,  test_tree_y = split_data(tree_x, y)
+
+# # define the inbalanced learning function
+# smote = SMOTENC(random_state=42)
+
+# # create new no show cases to balance the data sets
+# train_linear_x, train_linear_y = smote.fit_resample(train_linear_x, train_linear_y)
+# train_tree_x, train_tree_y = smote.fit_resample(train_tree_x, train_tree_y)
 
 # define the objective function
 objective_fn = get_f1_score
@@ -27,11 +36,45 @@ results = find_best_models(train_linear_x, train_tree_x, train_linear_y, train_t
 print("Objective Function: F1 Score")
 print()
 
-# prints the stats
-lin_model, y_lin_pred = print_stats(results, test_linear_x, test_linear_y, "Logistic Regression")
+print("Logistic Regression")
+print()
 
-# find the prediction probability
-y_lin_score = lin_model.predict_proba(test_linear_x)[:, 1]
+# # prints the stats
+# lin_model, y_lin_pred = print_stats(results, test_linear_x, test_linear_y, "Logistic Regression")
+
+# # find the prediction probability
+# y_lin_score = lin_model.predict_proba(test_linear_x)[:, 1]
+
+lin_model = results["Logistic Regression"][0]
+
+y_lin_score = lin_model.predict_prob(test_linear_x)[:, 1]
+
+best_threshold = 0.5
+best_f1 = -1
+
+thresholds = np.arange(0.05, 0.96, 0.05)
+scores = []
+
+for threshold in thresholds:
+    y_lin_pred = (y_lin_score >= threshold).astype(int)
+
+    f1 = evaluate_model(test_linear_y, y_lin_pred, get_f1_score)
+
+    if f1 > best_f1:
+        best_f1 = f1
+        best_threshold = threshold
+
+print(f"Best Threshold: {best_threshold:.2f}")
+print(f"Best F1: {best_f1:.4f}")
+print()
+
+y_lin_pred = (y_lin_score >= threshold).astype(int)
+
+print("Balanced Accuracy:", evaluate_model(test_linear_y, y_lin_pred, get_balanced_accuracy))
+print("Precision:", evaluate_model(test_linear_y, y_lin_pred, get_precision))
+print("Recall:", evaluate_model(test_linear_y, y_lin_pred, get_recall))
+print("F1 Score:", evaluate_model(test_linear_y, y_lin_pred, get_f1_score))
+print("MCC:", evaluate_model(test_linear_y, y_lin_pred, get_mcc))
 
 # plot graphs
 plot_confusion_matrix(test_linear_y, y_lin_pred, "Logistic Regression")
@@ -45,11 +88,45 @@ tree_names = ["Random Forest", "Histogram-based Gradient Boosting", "Extreme Gra
 # loops through all 3 tree-based models
 for tree_name in tree_names:
 
-    # prints the stats
-    model, y_pred = print_stats(results, test_tree_x, test_tree_y, tree_name)
+    print(tree_name)
+    print()
 
-    # find the prediction probability
-    y_score = model.predict_proba(test_tree_x)[:, 1]
+    # # prints the stats
+    # model, y_pred = print_stats(results, test_tree_x, test_tree_y, tree_name)
+
+    # # find the prediction probability
+    # y_score = model.predict_proba(test_tree_x)[:, 1]
+
+    model = results[tree_name][0]
+
+    y_score = model.predict_prob(test_tree_x)[:, 1]
+
+    best_threshold = 0.5
+    best_f1 = -1
+
+    thresholds = np.arange(0.05, 0.96, 0.05)
+    scores = []
+
+    for threshold in thresholds:
+        y_pred = (y_score >= threshold).astype(int)
+
+        f1 = evaluate_model(test_tree_y, y_pred, get_f1_score)
+
+        if f1 > best_f1:
+            best_f1 = f1
+            best_threshold = threshold
+
+    print(f"Best Threshold: {best_threshold:.2f}")
+    print(f"Best F1: {best_f1:.4f}")
+    print()
+
+    y_pred = (y_score >= threshold).astype(int)
+
+    print("Balanced Accuracy:", evaluate_model(test_tree_y, y_pred, get_balanced_accuracy))
+    print("Precision:", evaluate_model(test_tree_y, y_pred, get_precision))
+    print("Recall:", evaluate_model(test_tree_y, y_pred, get_recall))
+    print("F1 Score:", evaluate_model(test_tree_y, y_pred, get_f1_score))
+    print("MCC:", evaluate_model(test_tree_y, y_pred, get_mcc))
 
     # plot graphs
     plot_confusion_matrix(test_tree_y, y_pred, tree_name)
